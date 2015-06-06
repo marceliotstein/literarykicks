99b08d527ca747bed999f142113505811042002a
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-06-06 09:32:24 -0400

    new listing format now adopted for topic pages (in progress)

--- sites/all/themes/litstrap/templates/node--view--taxonomy-term.tpl.php
+++ sites/all/themes/litstrap/templates/node--view--taxonomy-term.tpl.php
@@ -1,44 +1,50 @@
 <?php
-  // primary article (story) node template for litkicks
+// primary article (story) node template for litkicks
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <?php print $user_picture; ?>
-
-  <?php print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h1<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h1>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-
   <?php
   $author = $node->name;
   $createdate = format_date($node->created, 'custom', "F jS, Y");
-  $byline = $author . " &#8226; " . $createdate;
-
-  print "<div class=\"article-by-line\">" . $byline . "</div>";
+  $img = '<img src="http://litkicks.com/sites/default/files/' . $node->field_story_image['und'][0]['filename'] . '" />';
   ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-
-      $rc = render($content);
-      // HACK FOR LOCALHOST
-      if (is_localhost()) {
-        $rc = str_replace("/sites/default/files","/litkicks/sites/default/files",$rc);
-        $rc = str_replace("\"sites/default/files","\"/litkicks/sites/default/files",$rc);
-      }
-      // END OF LOCALHOST HACK
-      print $rc;
-    ?>
+  <div class="col-xs-12 col-sm-12 hidden-md hidden-lg hidden-xl">
+    <div class="lklisting-pic"><?php print $img ?></div>
+    <div class="lklisting-title"><?php print $title ?></div>
+    <div class="lklisting-date"><?php print $createdate ?></div>
+    <div class="lklisting-author"><?php print $author ?></div>
   </div>
-  <br /><br />
+  <div class="hidden-xs hidden-sm col-md-12 col-lg-12 col-xl-12">
+    <table class="lklisting-table">
+      <tr class="lklisting-tablerow">
+        <td class="lklisting-pic">
+          <?php print $img ?>
+        </td>
+        <td class="lklisting-desc">
+          <div class="lklisting-title"><?php print $title ?></div>
+          <div class="lklisting-date"><?php print $createdate ?></div>
+          <div class="lklisting-author"><?php print $name ?></div>
+          <!-- AddThis BEGIN -->
+          <div class="addthis_sharing_toolbox"></div>
+          <!-- AddThis END -->
+        </td>
+      </tr>
+      <tr colspan="2">
+        <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
 
-  <!-- AddThis BEGIN -->
-  <div class="addthis_sharing_toolbox"></div>
-  <!-- AddThis END -->
-  <br />
+        $rc = render($content);
+        // HACK FOR LOCALHOST
+        if (is_localhost()) {
+          $rc = str_replace("/sites/default/files","/literarykicks/sites/default/files",$rc);
+          $rc = str_replace("\"sites/default/files","\"/literarykicks/sites/default/files",$rc);
+        }
+        // END OF LOCALHOST HACK
+        print $rc;
+        ?>
+      </tr>
+    </table>
+  </div>
 </div>
