52bb49ed55d3f143607ca6679a9f927b8ed3cb3a
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-06-06 10:04:36 -0400

    more topic listing styling

--- sites/all/themes/litstrap/templates/node--view--taxonomy-term.tpl.php
+++ sites/all/themes/litstrap/templates/node--view--taxonomy-term.tpl.php
@@ -3,17 +3,25 @@
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <?php
+  $nodeurl = 'node/' . $node->nid;
+  $thisalias = drupal_get_path_alias($nodeurl);
+  if (is_localhost()) {
+    $thisurl = "/literarykicks/" . $thisalias;
+  } else {
+    $thisurl = "/" . $thisalias;
+  }
+  $titlelink = '<a href="' . $thisurl . '">' . $title . '</a>';
   $author = $node->name;
   $createdate = format_date($node->created, 'custom', "F jS, Y");
   if (empty($node->field_story_image)) {
     $img = '<img src="http://litkicks.com/images/pv_good.gif" width="190" height="120" />';
   } else {
-    $img = '<img src="http://litkicks.com/sites/default/files/' . $node->field_story_image['und'][0]['filename'] . '" />';
+    $img = '<a href="' . $thisurl . '"><img src="http://litkicks.com/sites/default/files/' . $node->field_story_image['und'][0]['filename'] . '" /></a>';
   }
   ?>
 
   <div class="col-xs-12 col-sm-12 hidden-md hidden-lg hidden-xl lklisting-entry">
-    <div class="lklisting-title"><?php print $title ?></div>
+    <div class="lklisting-title"><?php print $titlelink ?></div>
     <div class="lklisting-pic"><?php print $img ?></div>
     <div class="lklisting-date"><?php print $createdate ?></div>
     <div class="lklisting-author">by <?php print $author ?></div>
@@ -25,7 +33,7 @@
           <?php print $img ?>
         </td>
         <td class="lklisting-desc">
-          <div class="lklisting-title"><?php print $title ?></div>
+          <div class="lklisting-title"><?php print $titlelink ?></div>
           <div class="lklisting-date"><?php print $createdate ?></div>
           <div class="lklisting-author">by <?php print $name ?></div>
         </td>
