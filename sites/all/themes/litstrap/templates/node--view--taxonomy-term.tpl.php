f9b9daed84718b50f9a128b713133ccc50608364
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-06-06 09:47:31 -0400

    further topic page listing tweaks

--- sites/all/themes/litstrap/templates/node--view--taxonomy-term.tpl.php
+++ sites/all/themes/litstrap/templates/node--view--taxonomy-term.tpl.php
@@ -16,36 +16,15 @@
   </div>
   <div class="hidden-xs hidden-sm col-md-12 col-lg-12 col-xl-12 lklisting-entry">
     <table>
-      <tr colspan="2">
-        <div class="lklisting-title"><?php print $title ?></div>
-      </tr>
       <tr>
         <td class="lklisting-pic">
           <?php print $img ?>
         </td>
         <td class="lklisting-desc">
+          <div class="lklisting-title"><?php print $title ?></div>
           <div class="lklisting-date"><?php print $createdate ?></div>
           <div class="lklisting-author">by <?php print $name ?></div>
-          <!-- AddThis BEGIN -->
-          <div class="addthis_sharing_toolbox"></div>
-          <!-- AddThis END -->
         </td>
-      </tr>
-      <tr colspan="2">
-        <?php
-        // We hide the comments and links now so that we can render them later.
-        hide($content['comments']);
-        hide($content['links']);
-
-        $rc = render($content);
-        // HACK FOR LOCALHOST
-        if (is_localhost()) {
-          $rc = str_replace("/sites/default/files","/literarykicks/sites/default/files",$rc);
-          $rc = str_replace("\"sites/default/files","\"/literarykicks/sites/default/files",$rc);
-        }
-        // END OF LOCALHOST HACK
-        print $rc;
-        ?>
       </tr>
     </table>
   </div>
