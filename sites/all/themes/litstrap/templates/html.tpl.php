8b2ef38407d9918ef4b5ceb7b009e698b424fd75
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-06-20 12:29:55 -0400

    rearrange new external link script order

--- sites/all/themes/litstrap/templates/html.tpl.php
+++ sites/all/themes/litstrap/templates/html.tpl.php
@@ -57,9 +57,9 @@
   <!--[if lt IE 9]>
     <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
-  <script src="/sites/all/themes/litstrap/js/external_links.js"></script>
   <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=asheresque" async="async"></script>
   <?php print $scripts; ?>
+  <script src="/sites/all/themes/litstrap/js/external_links.js"></script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
