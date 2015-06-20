79d49c2a83e9ba655500c9e3ed3e43d295914e90
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-06-20 12:25:31 -0400

    fixed jQuery $ for external links

--- sites/all/themes/litstrap/js/external_links.js
+++ sites/all/themes/litstrap/js/external_links.js
@@ -1,7 +1,7 @@
-$('a').each(function() {
+jQuery('a').each(function() {
    var a = new RegExp('/' + window.location.host + '/');
    if(!a.test(this.href)) {
-       $(this).click(function(event) {
+       jQuery(this).click(function(event) {
            event.preventDefault();
            event.stopPropagation();
            window.open(this.href, '_blank');
